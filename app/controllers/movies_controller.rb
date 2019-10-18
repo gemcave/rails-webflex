e94f556325502592a18f083f47756a6b8617637d
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-18 23:23:29 +0600

    edit movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,19 @@ class MoviesController < ApplicationController
 		@movie = Movie.find(params[:id])
 	end
 
+
+	def update
+		@movie = Movie.find(params[:id])
+
+		if @movie.update(movie_params)
+			redirect_to @movie
+		else
+			render :edit
+		end
+	end
+
+	private
+	def movie_params
+		params.require(:movie).permit(:title,:total_gross, :description, :released_on)
+	end
 end
