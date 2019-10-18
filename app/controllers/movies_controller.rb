98de361d2e286f5276595817efbdaaef29b3f43d
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-19 03:46:00 +0600

    destroy movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,13 +25,22 @@ class MoviesController < ApplicationController
 	def new
 		@movie = Movie.new
 	end
-	
+
 	def create
 		@movie = Movie.new(movie_params)
 		if @movie.save
 			redirect_to @movie
 		else
 			render :new
+		end
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		if @movie.destroy
+			redirect_to movies_url
+		else
+			render :show
 		end
 	end
 	private
