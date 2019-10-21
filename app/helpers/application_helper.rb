224b8885929b50acb5ecb222869cc1ca155d1c4f
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-21 22:58:32 +0600

    helper methods for specific content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
 	def page_title
-    if content_for?(:title)
-      "Flix - #{content_for(:title)}"
-    else
-      "Flix"
-    end
-  end
+		if content_for?(:title)
+			content_tag(:title, "Flix - #{content_for(:title)}")
+		else
+			content_tag(:title, "Flix")
+		end
+	end
+	
+	def title(title)
+		content_for(:title, title)
+	end
 end
