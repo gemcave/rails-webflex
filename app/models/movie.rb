c7e95d6e8d4a7acfc643b13415d7bad9eb145a15
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-10-22 00:01:05 +0600

    custom scope methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,8 @@ class Movie < ApplicationRecord
 		message: "must reference a GIF, JPG, or PNG image"
 	}
 
-	has_many :reviews, dependent: :destroy
+	has_many :reviews, -> { order(created_at: :desc) }, dependent: :destroy
+
 	has_many :favourites, dependent: :destroy
 	has_many :fans, through: :favourites, source: :user
 	has_many :characterizations, dependent: :destroy
@@ -23,13 +24,16 @@ validates :rating, inclusion: { in: RATINGS }
 		where("released_on <= ?", Time.now).order("released_on desc")
 	end
 
-	def self.hits
-		where('total_gross >= 300000000').order(total_gross: :desc)
-	end
-
-	def self.flops
-		where('total_gross < 50000000').order(total_gross: :asc)
-	end
+	scope :hits, -> { released.where('total_gross >= 300000000').order(total_gross: :desc) }
+	scope :flops, -> { released.where('total_gross < 50000000').order(total_gross: :asc) }
+	scope :upcoming, -> {  where("released_on > ?", Time.now).order(released_on: :asc) }
+	scope :recent, ->(max=5) { released.limit(max) }
+	scope :rated, ->(rating) { released.where(rating: rating) }
+	scope :by_name, -> { order(:name) }
+	scope :not_admins, -> { by_name.where(admin: false) }
+	scope :past_n_days, ->(days) { where('created_at >= ?' , days.days.ago) }
+	scope :grossed_less_than, ->(amount) { released.where('total_gross < ?', amount) }
+	scope :grossed_greater_than, ->(amount) { released.where('total_gross > ?', amount) }
 
 	def self.recently_added
 		order('created_at desc').limit(3)
